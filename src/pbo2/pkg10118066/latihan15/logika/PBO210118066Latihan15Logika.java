4b5144b186eaea6ee8abee1b7764bfd89fbec106
Tegar Lucky'Q Oakley <55655976+tegaroakley@users.noreply.github.com>
2019-09-24 16:01:13 +0900

    Add files via upload

---
+++ src/pbo2/pkg10118066/latihan15/logika/PBO210118066Latihan15Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118066.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Tegar Lucky'Q Oakley 
+ * NIM   : 10118066 
+ * KELAS : IF2 
+ * Deskripsi Program : Program ini berisi program untuk Menunjukkan sistem operasi Logika 
+ */
+public class PBO210118066Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b    = " + (a && b));
+        System.out.println("a || b    = " + (a || b));
+        System.out.println("!a        = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
